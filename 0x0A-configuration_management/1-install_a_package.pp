7aad975ace5863e4b0a600b707ce009c6a8310a2
essaket <hichamessaket17gmail.com>
2024-04-19 12:23:24 -0700

    Writing task 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,5 @@
-#!/usr/bin/pup
 # Install an especific version of flask (2.1.0)
-package { 'flask':
-  ensure    => '2.1.0',
-  provider  => 'pip3'
+package { 'install_flask':
+  command => '/usr/bin/pip3 install flask==2.1.0',
+  unless  => '/usr/bin/pip3 show flask | grep -q "Version: 2.1.0"',
 }
