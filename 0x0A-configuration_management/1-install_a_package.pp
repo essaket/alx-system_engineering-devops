e1dfdb901fc9b76e92e5b98facb09260b2ae62a6
Hicham Essaket <132906055+essaket@users.noreply.github.com>
2024-04-19 19:18:44 +0100

    Writing task 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,6 @@
-# Install flask from pip3 by using Puppet
-package { 'flask':
+#!/usr/bin/pup
+# Install an especific version of flask (2.1.0)
+package {'flask':
   ensure   => '2.1.0',
-  provider => 'pip'
+  provider => 'pip3'
 }
