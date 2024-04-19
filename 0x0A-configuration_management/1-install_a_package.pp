a16569b410e511845072ccdb9a509cc75038e380
essaket <hichamessaket17gmail.com>
2024-04-19 12:30:16 -0700

    Writing task 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,6 @@
-# Install an especific version of flask (2.1.0)
-package { 'install_flask':
-  command => '/usr/bin/pip3 install flask==2.1.0',
-  unless  => '/usr/bin/pip3 show flask | grep -q "Version: 2.1.0"',
+# A puppet manifest installing flask v2.1.0 which is a package from pip3.
+# Am also specifying the version of flask to install using the ensure attribute
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => pip3,
 }
