049fe75e5a8ad8a3495d7e9b32dfb043a0b31dae
essaket <hichamessaket17gmail.com>
2024-04-29 14:48:28 -0700

    Writing task 2

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -2,17 +2,22 @@
 
 exec {'update':
   command  => 'sudo apt-get -y update',
+  require   => Exec['install Nginx'],
 }
 
 exec {'install Nginx':
   command  => 'sudo apt-get -y install nginx',
+  require   => Exec['add_header'],
 }
 
 exec { 'add_header':
   environment => ["HOST=${hostname}"],
   command     => 'sudo sed -i "s/include \/etc\/nginx\/sites-enabled\/\*;/include \/etc\/nginx\/sites-enabled\/\*;\n\tadd_header X-Served-By \"$HOST\";/" /etc/nginx/nginx.conf',
+  require      => Exec['restart Nginx'],
 }
 
 exec { 'restart Nginx':
   command  => 'sudo service nginx restart',
 }
+
+r
