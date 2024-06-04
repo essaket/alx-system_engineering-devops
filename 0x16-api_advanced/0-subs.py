5796d691759a87394b8a9ca7454ef8227c59fb94
Hicham Essaket <132906055+essaket@users.noreply.github.com>
2024-06-04 22:54:13 +0100

    Writing task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,21 +1,16 @@
 #!/usr/bin/python3
-"""queries the Reddit API"""
+"""Function to query subscribers on a given Reddit subreddit."""
 import requests
 
 
 def number_of_subscribers(subreddit):
-  """returns total subs"""
-  header = {"User-Agent": "Holberton"}
-  url = "https://www.reddit.com/r/" + subreddit + "/about.json"
-  try:
-    r = requests.get(url, headers=header, allow_redirects=False)
-    r.raise_for_status()  # Raise an exception for non-200 status codes
-  except requests.exceptions.RequestException as e:
-    print(f"Error: {e}")
-    return 0
-  else:
-    # Successful request processing
-    return r.json().get("data", None).get("subscribers", None)
-
-if __name__ == "__main__":
-  pass
+    """Return the total number of subscribers on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
+        return 0
+    results = response.json().get("data")
+    return results.get("subscribers")
