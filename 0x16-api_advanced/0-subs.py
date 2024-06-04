af2e32977945b43bca30edd4e40c8cbea2ea44ed
essaket <hichamessaket17@gmail.com>
2024-06-04 09:29:17 -0700

    Writing task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -11,8 +11,6 @@ import requests
 def number_of_subscribers(subreddit):
     """returns number of total subscribers"""
     url = ("https://api.reddit.com/r/{}/about".format(subreddit))
-    headers = {'User-Agent': 'CustomClient/1.0'}
-    response = requests.get(url, headers=headers, allow_redirects=False)
 
     if response.status_code != 200:
         return (0)
