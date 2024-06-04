7ae8798c5b2fdecfb55e487cb90870d761d12972
essaket <hichamessaket17@gmail.com>
2024-06-04 13:14:59 -0700

    Writing task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python3
-"""Module looking for the number of subscribers to a subreddit"""
-import requests
+""" script to obtain subscribers
+    count from a subreddit
+"""
+from requests import get
 
 
 def number_of_subscribers(subreddit):
-    """Search the number of subscribers to a subreddit"""
-    url = f'https://www.reddit.com/r/{subreddit}/about.json'
-    user = {'User-Agent': 'essaket'}
-    response = requests.get(url, headers=user, allow_redirects=False)
-    if response.status_code == 404:
-        return 0
-    return response.json().get('data').get('subscribers')
+    """ function to get subscriber count"""
+    if subreddit and type(subreddit) is str:
+        subscribers = 0
+        url = 'https://reddit.com/r/{}/about.json'.format(subreddit)
+        headers = {'user-agent': 'my-app/0.0.1'}
+        req = get(url, headers=headers)
+        if req.status_code == 200:
+            data = req.json()
+            subscribers = data.get('data', {}).get('subscribers', 0)
+        return subscribers
