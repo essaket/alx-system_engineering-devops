014d8cf9ff61cbedab7bd5a1706c7c6b2ad17c19
essaket <hichamessaket17@gmail.com>
2024-06-04 12:48:32 -0700

    Writing task 1

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Top Ten"""
+import requests
+
+
+def top_ten(subreddit):
+    """Function that queries the Reddit API and prints the titles of the
+    first 10 hot posts listed for a given subreddit"""
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    headers = {"User-Agent": "1-top_ten.py"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200 and response.text.strip():
+        for post in response.json().get("data").get("children"):
+            print(post.get("data").get("title"))
+    else:
+        print(None)
