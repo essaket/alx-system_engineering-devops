6434d6989cec4a7094b2522ead41c249c9ce2222
essaket <hichamessaket17@gmail.com>
2024-06-04 14:30:51 -0700

    Writing task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,14 +4,18 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    """returns total subs"""
-    header = {"User-Agent": "Holberton"}
-    url = "https://www.reddit.com/r/" + subreddit + "/about.json"
-    r = requests.get(url, headers=header)
-    if r.status_code == 200:
-        return r.json().get("data", None).get("subscribers", None)
-    else:
-        return 0
+  """returns total subs"""
+  header = {"User-Agent": "Holberton"}
+  url = "https://www.reddit.com/r/" + subreddit + "/about.json"
+  try:
+    r = requests.get(url, headers=header, allow_redirects=False)
+    r.raise_for_status()  # Raise an exception for non-200 status codes
+  except requests.exceptions.RequestException as e:
+    print(f"Error: {e}")
+    return 0
+  else:
+    # Successful request processing
+    return r.json().get("data", None).get("subscribers", None)
 
 if __name__ == "__main__":
-    pass
+  pass
